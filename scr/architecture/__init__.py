fe41970d2ed797e18dd22206ec89cfa2e8104ffc
poco40 <128311098+companys1234@users.noreply.github.com>
2025-12-30 13:36:23 +0500

    Update __init__.py
    починили импорт

--- scr/architecture/__init__.py
+++ scr/architecture/__init__.py
@@ -1,6 +1,6 @@
-from Swiglu import SwigLU
-from RMSNorm import RMSNorm
-from GMQA_with_KV import GMQA_with_KV
-from Feed_Foward_Network import Feed_Forward_Network
+from .Swiglu import SwigLU
+from .RMSNorm import RMSNorm
+from .GMQA_with_KV import GMQA_with_KV
+from .Feed_Foward_Network import Feed_Forward_Network
 
 __all__ = ['SwigLU', 'RMSNorm', 'GMQA_with_KV', 'Feed_Forward_Network']
