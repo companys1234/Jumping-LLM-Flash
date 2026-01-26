d2fb221f537ee67818df20791fe3b17b7f2d00e3
poco40 <128311098+companys1234@users.noreply.github.com>
2026-01-26 17:14:38 +0500

    Update __init__.py

--- scr/preprocessing/__init__.py
+++ scr/preprocessing/__init__.py
@@ -1,5 +1,6 @@
 from .tokenizer import SimpleTokenizer
 from .BPE import BPE
+from .SentencePiece import SentencePieceBPE
+__all__ = ['SimpleTokenizer', 'BPE', 'SentencePieceBPE']
 
-__all__ = ['SimpleTokenizer', 'BPE']
 
